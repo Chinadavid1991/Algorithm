20cd80388f5149710dd2994e643cb3780d0a45f1
Chinadavid1991 <zxfheu@126.com>
2020-12-15 08:38:37 +0800

    11-26

--- main.cpp
+++ main.cpp
@@ -2,8 +2,8 @@
 #include "SelectSort.h"
 using namespace std;
 int main() {
-    int data[16] = {10,9,5,6,7,8,4,3,2,1,11,13,12,16,15,14};
-    SelectSort<int, 16> sel(data);
+    int data[20] = {10,9,5,6,7,8,4,3,2,1,11,13,12,16,15,14,17,18,19,20};
+    SelectSort<int, sizeof(data)/ sizeof(int)> sel(data);
     sel.sort();
     for(int x : data){
         cout << x << endl;
