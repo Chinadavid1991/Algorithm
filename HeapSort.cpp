9c764d1f7b3b5814dd0ec950576563cf9a748faa
feng <zxfheu@126.com>
2020-12-23 09:58:08 +0800

    p2:12-23

--- HeapSort.cpp
+++ HeapSort.cpp
@@ -3,17 +3,18 @@
 //
 
 #include "HeapSort.h"
-void testHeapSort(){
+
+void testHeapSort() {
     int arr[101] = {0};
-    for(int ix = 0;ix <= 10;++ix){
-        if(ix % 10 == 0){
+    for (int ix = 0; ix <= 10; ++ix) {
+        if (ix % 10 == 0) {
             arr[ix] = 10;
         }
-        else{
+        else {
             arr[ix] = ix;
         }
     }
-    HeapSort<int> sort(arr,11,1000);
+    HeapSort<int> sort(arr, 11, 1000);
     sort.sort();
     sort.print();
 }
